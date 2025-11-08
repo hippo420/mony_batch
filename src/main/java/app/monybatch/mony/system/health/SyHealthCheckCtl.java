753c5b9f41a761305944e8bcd7baee01a6344648
hippo420 <hippo420@naver.com>
2025-11-08 17:37:28 +0900

    수정

--- src/main/java/app/monybatch/mony/system/health/SyHealthCheckCtl.java
+++ src/main/java/app/monybatch/mony/system/health/SyHealthCheckCtl.java
@@ -1,12 +1,14 @@
 package app.monybatch.mony.system.health;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@Slf4j
 @RestController("/system")
 public class SyHealthCheckCtl {
     @GetMapping("/health")
     public String heathCheck() {
+        log.info("Health check started");
         return "OK";
     }
 }
